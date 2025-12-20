2952694d48d7d93226bcac0e95c84da9938d5a70
boyoyon <nishimaki.hidetoshi@gmail.com>
2025-12-20 15:47:04 +0900

    Add files via upload

--- src/single_step_erode_rgb_depth_to_pcd.py
+++ src/single_step_erode_rgb_depth_to_pcd.py
@@ -48,12 +48,15 @@ def main():
     argc = len(argv)
 
     print('%s creates ply from rgb_image and depth_image' % argv[0])
-    print('[usage] python %s <rgb image> <depth(.npy)> <intrinsic(.npy)> <extrinsic(.npy)>' % argv[0])
+    print('[usage] python %s <folder> <number>' % argv[0])
     
-    if argc < 5:
+    if argc < 3:
         quit()
 
-    rgb = cv2.imread(argv[1])
+    folder = argv[1]
+    no = int(argv[2])
+
+    rgb = cv2.imread(os.path.join(folder, 'rgb_%04d.png' % no))
     H, W = rgb.shape[:2]
     gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
 
@@ -63,17 +66,15 @@ def main():
     kernel = np.ones((KERNEL_SIZE, KERNEL_SIZE), np.uint8)
     mask = cv2.erode(mask, kernel, iterations=1)
 
-    depth = np.load(argv[2])
+    depth = np.load(os.path.join(folder, 'depth_%04d.npy' % no))
 
-    intrinsic = np.load(argv[3])
-    fx = intrinsic[0][0]
-    fy = intrinsic[1][1]
-    cx = intrinsic[0][2]
-    cy = intrinsic[1][2]
+    K = np.load(os.path.join(folder, 'intrinsic_%04d.npy' % no))
+    K_inv = np.linalg.inv(K)
 
-    extrinsic = np.load(argv[4])
-    R = extrinsic[:,0:3]
-    T = extrinsic[:,3]
+    E = np.load(os.path.join(folder, 'extrinsic_%04d.npy' % no))
+    R = E[:,0:3]
+    R_inv = R.T
+    t = E[:,3]
 
     lines = []
     for y in range(H):
@@ -85,23 +86,20 @@ def main():
             r = rgb[y][x][2]
             g = rgb[y][x][1]
             b = rgb[y][x][0]
-            zc = depth[y][x]
-            xc = (x - cx) * zc / fx
-            yc = (y - cy) * zc / fy
+            d = depth[y][x]
 
-            C = np.array([xc,yc,zc]).T
-            w = R@C+T
+            Coord_pixel = np.array([x/d, y/d, 1.0])
+            Coord_camera = d * (K_inv @ Coord_pixel)
+            Coord_world = R_inv @ (Coord_camera - t)
 
-            xw = w[0]
-            yw = w[1]
-            zw = w[2]
+            xw = Coord_world[0]
+            yw = Coord_world[1]
+            zw = Coord_world[2]
 
             line = '%f %f %f %d %d %d\n' % (xw, yw, zw, r, g, b)
             lines.append(line)
 
-    base = os.path.basename(argv[1])
-    filename = os.path.splitext(base)[0]
-    ply_path = '%s_erode_%d.ply' % (filename, KERNEL_SIZE)
+    ply_path = 'rgb_%04d_erode_%d.ply' % (no, KERNEL_SIZE)
     save_ply(ply_path, lines)
     print('save %s' % ply_path)
 
